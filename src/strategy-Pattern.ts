32ac20967169e656479caec24f28e886433c5a51
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-04-19 16:26:33 +0530

    ✏️: started: strategy pattern

---
+++ src/strategy-Pattern.ts
@@ -0,0 +1,102 @@
+//! Visitor Pattern -- least used
+
+/* The Visitor Pattern in TypeScript is a behavioral design pattern that
+  allows you to separate algorithms from the objects on which they operate. */
+
+type Listener<EventType> = (ev: EventType) => void;
+
+function createObserver<EventType>(): {
+  subscribe: (listener: Listener<EventType>) => () => void;
+  publish: (event: EventType) => void;
+} {
+  const listeners: Listener<EventType>[] = [];
+
+  return {
+    subscribe: (listener: Listener<EventType>): (() => void) => {
+      listeners.push(listener);
+      return () => {
+        const listenerIndex = listeners.indexOf(listener);
+        listeners.splice(listenerIndex, 1);
+      };
+    },
+    publish: (event: EventType) => {
+      listeners.forEach((listener) => listener(event));
+    },
+  };
+}
+
+interface BeforeSetEvent<T> {
+  value: T;
+  newValue: T;
+}
+
+interface AfterSetEvent<T> {
+  value: T;
+}
+
+interface Pokemon {
+  id: string;
+  attack: number;
+  defense: number;
+}
+
+interface BaseRecord {
+  id: string;
+}
+
+interface Database<T extends BaseRecord> {
+  set(newValue: T): void;
+  get(id: string): T | undefined;
+
+  onBeforeAdd(listener: Listener<BeforeSetEvent<T>>): () => void;
+  onAfterAdd(listener: Listener<AfterSetEvent<T>>): () => void;
+
+  visit(visitor: (item: T) => void): void;
+
+  selectBest(scoreStrategy: (item: T) => number): T | undefined;
+}
+
+function createDatabase<T extends BaseRecord>() {
+  class InMemoryDatabase implements Database<T> {
+    private db: Record<string, T> = {};
+    private beforeAddListeners = createObserver<BeforeSetEvent<T>>();
+    private afterAddListeners = createObserver<AfterSetEvent<T>>();
+    static instance: InMemoryDatabase = new InMemoryDatabase();
+
+    private constructor() {}
+
+    public set(newValue: T): void {
+      this.beforeAddListeners.publish({
+        value: this.db[newValue.id],
+        newValue: newValue,
+      });
+      this.db[newValue.id] = newValue;
+      this.afterAddListeners.publish({
+        value: newValue,
+      });
+    }
+    public get(id: string): T {
+      return this.db[id];
+    }
+    onBeforeAdd(listener: Listener<BeforeSetEvent<T>>): () => void {
+      return this.beforeAddListeners.subscribe(listener);
+    }
+    onAfterAdd(listener: Listener<AfterSetEvent<T>>): () => void {
+      return this.afterAddListeners.subscribe(listener);
+    }
+    visit(visitor: (item: T) => void): void {
+      Object.values(this.db).forEach(visitor);
+    }
+
+    selectBest(scoreStrategy: (item: T) => number): T | undefined {
+      return Object.values(this.db).reduce((best, current) =>
+        scoreStrategy(current) > scoreStrategy(best) ? current : best
+      );
+    }
+  }
+  return InMemoryDatabase;
+}
+
+const pokemonDB = createDatabase<Pokemon>();
+
+export { pokemonDB };
