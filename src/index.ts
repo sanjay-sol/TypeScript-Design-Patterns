a77dc790eee7729f3b2954ad2adc2384718a2abc
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-04-19 14:39:14 +0530

    ADD: interfaces ad records

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, world!')
+import { RecordHandler, loader } from "./loader";
+
+interface Pokemon {
+    id: string,
+    attack: number,
+    defense: number,
+}
+
+interface BaseRecord {
+    id: string;
+}
+
+interface Database<T extends BaseRecord> {
+    set(newValue: T): void;
+    get(id: string): T;
+}
