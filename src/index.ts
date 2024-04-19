9d1c3c8e771a4cf90866a07b5e569a8b69cd6393
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-04-19 14:57:12 +0530

    📦:new class

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,20 @@ interface Database<T extends BaseRecord> {
     set(newValue: T): void;
     get(id: string): T;
 }
+class InMemoryDatabase<T extends BaseRecord> implements Database<T> {
+    private db: Record<string, T> = {};
+
+    public set(newValue: T): void {
+        this.db[newValue.id] = newValue;
+    }
+    public get(id: string): T {
+        return this.db[id];
+    }
+}
+
+const pokemonDB = new InMemoryDatabase<Pokemon>();
+
+pokemonDB.set({ id: "Bulbasaur", attack: 50, defense: 50 });
+
+const bulbasaur = pokemonDB.get("Bulbasaur");
+console.log(bulbasaur);
